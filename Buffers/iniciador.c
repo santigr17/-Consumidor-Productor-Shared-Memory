b5172c92ad7c68fea335bf6330100bc7d39e014d
Esteban Ferarios <ferarios95@gmail.com>
2020-06-21 10:02:14 -0600

    Se agregan comments

--- Buffers/iniciador.c
+++ Buffers/iniciador.c
@@ -14,7 +14,6 @@ int main(int argc, char *argv[])
 	buffer ctx;
 	errores scberr1 = SCB_OK;
 	mensaje carta;
-
 	if(argc != 3){
 		printf("Por favor indique nombre del buffer y cantidad de mensajes\n");
 		printf("Argumento 0: [%s]\n", argv[0]);
@@ -22,15 +21,10 @@ int main(int argc, char *argv[])
         printf("Argumento 2: [%s]\n", argv[2]);
         return(1);
 	}
-
     //Se convierte el segundo argumento capturado a unsigned int
-
     cantidad = atoi(argv[2]);
-
-
 	printf("Creando Buffer Cicular compartido: [%s]\n", argv[1]);
 	scberr1 = crear_buffer(argv[1], cantidad, sizeof(mensaje), &ctx, &ret);
-
     //Se prueba la info del buffer creado:
 
         int err = 0;
@@ -39,19 +33,13 @@ int main(int argc, char *argv[])
     	int semcon_carrera = 0;
 	    buffer_control inf;
 	    errores scberr2;
-
-
     while(1){
-      
-
-     system("clear");
+    system("clear");
     /* errores get_info(char *name, buffer_control *inf, int *semlleno, int *semvacio, int *semcon_carrera,int *semconsumidores,int *semproductores, int *err);*/
-	
-    
-    
     scberr2 = get_info_buffer(argv[1], &inf, &semlleno, &semvacio, &semcon_carrera, &err);
+	//Se verifica algun error
 	SCB_SAMPLE_CHECK_ERROR(SCB_OK, scberr2, err, 1);
-
+	// se printea
 	printf("Nombre: [%s]\n", argv[1]);
 	printf("Cabeza..............: [%u]\n", inf.cabeza);
 	printf("COLA................: [%u]\n", inf.cola);
@@ -59,7 +47,6 @@ int main(int argc, char *argv[])
 	printf("Capaciad total......: [%u]\n", inf.capacidad);
 	printf("Productores.........: [%u]\n", inf.productores);
 	printf("Consumidores........: [%u]\n", inf.consumidores);
-
 	printf("Semaforos..........: Lleno [%d] | Vacio [%d] | Con_carrera [%d]\n", semlleno, semvacio, semcon_carrera);
     sleep(2);
     }
--- Buffers/productor.c
+++ Buffers/productor.c
@@ -17,20 +17,19 @@ int main(int argc, char *argv[])
 	buffer ctx;
 	errores scberr = SCB_OK;
 
-	
 
-		check_error(scberr, ret, scberrormsgcreate);
-		printf("%s", scberrormsgcreate);
+    check_error(scberr, ret, scberrormsgcreate);
+	printf("%s", scberrormsgcreate);
 
-		printf("Ligando productor con el buffer: [%s]\n", argv[1]);
+	printf("Ligando productor con el buffer: [%s]\n", argv[1]);
 
-		scberr = ligar_buffer(&ctx, argv[1], &ret,1);
+    //SE LIGA PRODUCTOR CON BUFFER USANDO EL ARGUMENTO PASADO POR CONSOLA
+	scberr = ligar_buffer(&ctx, argv[1], &ret,1);
+    SCB_SAMPLE_CHECK_ERROR(SCB_OK, scberr, ret, 1);
+
     
-		SCB_SAMPLE_CHECK_ERROR(SCB_OK, scberr, ret, 1);
-
-    
 	
-    /*while(1){
+    while(1){
         printf("Soy un productor corriendo \n");
-    }*/
+    }
 }
