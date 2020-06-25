2cd6fd493c482ca3497b827f989a85a61dfdc50a
Esteban Ferarios <ferarios95@gmail.com>
2020-06-25 11:43:26 -0600

    Se agrega cambio para escoger modo de consumidor

--- Buffers/consumidor.c
+++ Buffers/consumidor.c
@@ -6,6 +6,22 @@
 #include <time.h>
 #include "buffercircular.h"
 #include "mensaje.h"
+#include <ncurses.h>
+
+//Función para capturar teclas
+
+int kbhit(void)
+{
+    int ch = getch();
+
+    if (ch != ERR) {
+        ungetch(ch);
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
 
 
 int main(int argc, char *argv[])
@@ -19,16 +35,51 @@ int main(int argc, char *argv[])
 
 	check_error(scberr, ret, scberrormsgcreate);
 	printf("%s", scberrormsgcreate);
+
+	//Se verifica entradas
+
+	if(argc != 3){
+		printf("ERROR: Por favor indique nombre del buffer y el tipo de consumidor m (manual) o a (automatico) \n");
+		printf("Argumento 0: [%s]\n", argv[0]);
+        printf("Argumento 1: [%s]\n", argv[1]);
+        printf("Argumento 2: [%s]\n", argv[2]);
+        return(1);
+	}
+	
+	
 	scberr = get_buffer(&ctx,argv[1], &err);
 	scberr = add_consumidor(&ctx,argv[1], &err);
 
-	
 
+	//Funciones para la captura de teclas
+	initscr();
+    cbreak();
+    noecho();
+    nodelay(stdscr, TRUE);
+    scrollok(stdscr, TRUE);
 	//SE LIGA PRODUCTOR CON BUFFER USANDO EL ARGUMENTO PASADO POR CONSOLA
 	SCB_SAMPLE_CHECK_ERROR(SCB_OK, scberr, ret, 1);
 	
-	while (1)
-	{
+
+
+
+
+	if ( !strcmp(argv[2], "manual") ){
+		while(1){
+			 if (kbhit()) {
+            printw("Se presionó una tecla ! y fue la : %d\n", getch());
+            refresh();
+             } else {
+            printw("Esperando tecla ...\n");
+            refresh();
+            sleep(2);
+            }
+		}
+	}
+	else if ( !strcmp(argv[2], "automatico") ){
+
+		while (1)
+		{
         mensaje msj;
 		buffer ctx;
 		system("clear");
@@ -37,7 +88,7 @@ int main(int argc, char *argv[])
 
 		//Se intenta leer
 		scberr = get_msg(&ctx, &msj, copyMessage, BLOCK, &ret);
-
+		printf("Mi modo es: [%s]\n", argv[2]);
         printf("Mi buffer es: [%s]\n", argv[1]);
 	    printf("Numero mágico: ..............: [%u]\n", msj.numero_magico);
 	    printf("Escrito por:................: [%u]\n", msj.pid);
@@ -45,5 +96,17 @@ int main(int argc, char *argv[])
 		info = localtime(&(msj.time));
 	    printf("A las: .................: [%s]\n", asctime(info) );
 		sleep(4);
+		}
+
 	}
+	else{
+			printf("ERROR: Por favor indique nombre del buffer y el tipo de consumidor m (manual) o a (automatico) \n");
+	}
+
+
+	
+
+
+	endwin();
+	return 0;
 }
